250c798a588a1b8501a61ee5eabcd44bc4656e8e
Faure-Thomas <56997397+Faure-Thomas@users.noreply.github.com>
2024-09-06 14:48:37 +0200

    fix: missing import of protectRoute middleware in cartRoutes

--- backend/routes/cart.route.js
+++ backend/routes/cart.route.js
@@ -1,6 +1,7 @@
 import express from 'express';
 
 import { getCartProducts, addToCart, removeAllFromCart, updateQuantity } from '../controllers/cart.controller.js';
+import { protectRoute } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
